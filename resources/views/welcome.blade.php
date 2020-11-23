481b15fdd63f850c3cc0b810b01b428f748a6eaf
Jose Capellan <Jose.Capellan@solvex.com.do>
2020-11-22 20:47:54 -0400

    modify page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Chatbot I.A - BotMan</title>
+        <title>Cape Chat</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css">
         <link href="css/chatbot.css" rel="stylesheet" type="text/css">
